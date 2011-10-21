f0ee7f3d1125473fd3c58d84323ee719edf00273
ytnobody <ytnobody@gmail.com>
2011-10-21 17:34:42 +0900

    initial commit

---
+++ lib/JSON/RPC2/Server/Declare.pm
@@ -0,0 +1,32 @@
+package JSON::RPC2::Server::Declare;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+JSON::RPC2::Server::Declare -
+
+=head1 SYNOPSIS
+
+  use JSON::RPC2::Server::Declare;
+
+=head1 DESCRIPTION
+
+JSON::RPC2::Server::Declare is
+
+=head1 AUTHOR
+
+ytnobody E<lt>ytnobody@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
